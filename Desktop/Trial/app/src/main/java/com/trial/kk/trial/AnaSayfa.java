64a5bec60ae6bc6e4a40c66469b4788a79d8c5fa
mcanolgun <mcanolgun59@gmail.com>
2019-01-17 12:41:30 +0300

    Initial commit

---
+++ Desktop/Trial/app/src/main/java/com/trial/kk/trial/AnaSayfa.java
@@ -0,0 +1,21 @@
+package com.trial.kk.trial;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mehmetcanolgun on 17.01.2019.
+ */
+
+public class AnaSayfa extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_hmp,container,false);
+    }
+}
---
+++ Desktop/Trial/app/src/main/java/com/trial/kk/trial/GeriSayim.java
@@ -0,0 +1,21 @@
+package com.trial.kk.trial;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mehmetcanolgun on 16.01.2019.
+ */
+
+public class GeriSayim extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gerisayim_layout,container,false);
+    }
+
+}
