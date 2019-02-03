db81f18823d44b41b435685178e109a27b5d683c
mcanolgun <mcanolgun59@gmail.com>
2019-02-03 17:14:16 +0300

    Konular güncellendi

--- Desktop/Trial/app/src/main/java/com/trial/kk/trial/DatabaseConnection.java
+++ Desktop/Trial/app/src/main/java/com/trial/kk/trial/DatabaseConnection.java
@@ -1,7 +1,12 @@
 package com.trial.kk.trial;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mehmetcanolgun on 3.02.2019.
@@ -30,5 +35,21 @@ public class DatabaseConnection {
             a = 1;
         }
         String command = "update konular set 'selected' = "+String.valueOf(a)+" where 'konu' = "+konu;
+        sqLiteDatabase.execSQL(command);
+    }
+
+    List<Integer> getState(){
+        String columns[] = {"selected"};
+        Cursor c = sqLiteDatabase.query("konular",columns,null,null,null,null,null);
+        List<Integer> list = new ArrayList<Integer>();
+        c.moveToFirst();
+        for (int i=0;i<c.getCount();i++){
+            //while(!c.isAfterLast()){
+            int item = c.getInt(0);
+            list.add(item);
+            c.moveToNext();
+        }
+        c.close();
+        return list;
     }
 }
