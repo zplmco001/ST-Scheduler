3d891f9567f5289d52264156821ed46c0e074de6
Kürşat Burak Farız <kburakfariz@gmail.com>
2019-02-03 03:50:41 +0300

    kbf

--- Desktop/Trial/app/src/main/java/com/trial/kk/trial/Konular.java
+++ Desktop/Trial/app/src/main/java/com/trial/kk/trial/Konular.java
@@ -18,7 +18,7 @@ public class Konular extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        ;
+
 
         View view = inflater.inflate(R.layout.konular_layout, container, false);
 
